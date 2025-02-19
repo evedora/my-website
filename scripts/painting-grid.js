5e2535068ddddcfd23f8436d9ec695af07e197bc
Matt Kasprowicz <kasprowiczmatt@gmail.com>
2025-02-18 19:55:10 -0500

    add painting captions

--- scripts/painting-grid.js
+++ scripts/painting-grid.js
@@ -3,57 +3,79 @@
 const paintings = [ 
     {
         name: 'Unknown',
+        material: 'Oil on Canvas',
+        dimensions: '16 x 20',
         year: 2020,
         filePath: '241101 IMG_5396-2.jpg'
     },
     {
         name: 'Claire and Miles playing poker',
+        material: 'Oil on Canvas',
+        dimensions: '24 x 24',
         year: 2021,
         filePath: 'Claire+Miles.jpg'
     },
     {
         name: 'Castelnou',
+        material: 'Oil on Canvas',
+        dimensions: '72 x 48',
         year: 2020,
         filePath: 'EveOShea_2.jpeg'
     },
     {
         name: 'Flowered Scene',
+        material: 'Oil on Canvas',
+        dimensions: '60 x 60',
         year: 2018,
         filePath: 'floweredscene.jpg'
     },
     {
         name: 'Lise Peron',
+        material: 'Oil on Canvas',
+        dimensions: '48 x 48',
         year: 2020,
         filePath: 'OSheaEve_LisePeron.jpeg'
     },
     {
         name: 'Pathway',
+        material: 'Oil on Canvas',
+        dimensions: '16 x 20',
         year: 2020,
         filePath: 'pathway.jpg'
     },
     {
         name: 'Power Street',
+        material: 'Oil on Canvas',
+        dimensions: '16 x 20',
         year: 2020, 
         filePath: 'PowerStreet.jpg'
     },
     
     {
         name: 'Swimmer',
+        material: 'Oil on Canvas',
+        dimensions: '6 x 12',
         year:2019,
         filePath:  'swimmer.jpg',
     },
     {
         name: 'Rockaway',
+        material: 'Oil on Canvas',
+        dimensions: '16 x 20',
         year:2018,
         filePath: 'Rockaway.jpg',
     },
     {
         name: 'The Aquarium',
+        material: 'Oil on Canvas',
+        dimensions: '36 x 24',
         year: 2020,
         filePath: 'Theaquarium.jpg',
     },
     {
         name: 'Winter Walk',
+        material: 'Oil on Canvas',
+        dimensions: '60 x 60',
         year: 2020, 
         filePath:  'winterwalk.jpg',
     },
@@ -67,6 +89,7 @@ paintings.forEach((painting) => {
 
     const thumbnailDiv = document.createElement('div')
     thumbnailDiv.classList.add('thumbnail-container')
+    thumbnailDiv.style.width = '300px'
     const image = document.createElement('img')
     image.classList.add('thumbnail')
     image.src = `painting/${painting.filePath}`
@@ -74,7 +97,7 @@ paintings.forEach((painting) => {
     
     const text = document.createElement('div')
     text.classList.add('caption-text')
-    text.textContent = `${painting.name} ${painting.year}`
+    text.textContent = `${painting.name}. ${painting.year}. ${painting.material}. ${painting.dimensions} inches`
     text.hidden = true
 
     thumbnailDiv.addEventListener('mouseenter', () => {
@@ -85,6 +108,7 @@ paintings.forEach((painting) => {
 
     thumbnailDiv.addEventListener('mouseleave', () => {
         text.hidden= true
+        
     })
 
     thumbnailDiv.appendChild(image)
